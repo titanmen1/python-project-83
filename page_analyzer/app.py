8c18c532b67c77ca879b607f67f1328f3142cb4b
Artem Arkhipov <aarhipov@broniboy.ru>
2023-10-21 13:44:39 +0400

    step 2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from dotenv import load_dotenv
+from flask import Flask, render_template
 
+load_dotenv()
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
