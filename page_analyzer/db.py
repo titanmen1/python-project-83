bf6facd90e57a40e07a3e3c519ace7904427e6b5
Artem Arkhipov <aarhipov@broniboy.ru>
2023-10-24 11:25:35 +0400

    step 6.3

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -34,7 +34,7 @@ def get_url_by_id(connection, url_id):
 
 def get_url_by_name(connection, url_name):
     with connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-        cursor.execute(f'SELECT id, name FROM urls WHERE name={url_name}')
+        cursor.execute(f"SELECT id, name FROM urls WHERE name='{url_name}'")
         url = cursor.fetchone()
     return url
 
